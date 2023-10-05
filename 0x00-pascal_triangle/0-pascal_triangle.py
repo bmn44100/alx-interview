3832c1df305abc326d3c574a95cbdc1f2ef8cf58
bmn44100 <njerumurimi@gmail.com>
2023-10-05 11:46:18 -0700

    Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 module for pascal's triangle
 """
 
+
 def pascal_triangle(n):
     """
     :type n: int
@@ -10,11 +11,11 @@ def pascal_triangle(n):
     """
     if n == 0:
         return []
-        
+
     triangle = [[1]]
-    for i in range(1,n):
+    for i in range(1, n):
         row = [1]
-        for j in range(1,i):
+        for j in range(1, i):
             row.append(triangle[i-1][j-1]+triangle[i-1][j])
         row.append(1)
         triangle.append(row)
