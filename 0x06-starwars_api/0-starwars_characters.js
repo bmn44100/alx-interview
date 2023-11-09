51778ccbc9cb8f38786ed04daa4a9b2e02f51b40
bmn44100 <njerumurimi@gmail.com>
2023-11-09 23:00:00 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+// let id = process.argv[2];
+// const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
 
 request(url, async function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    const characters = JSON.parse(body).characters;
+  const characters = JSON.parse(body).characters;
 
-    for (const character of characters) {
-        await new Promise(function (resolve, reject) {
-            request(character, function (error, response, body) {
-                if (error) {
-                    console.log(error);
-                }
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+  for (const character of characters) {
+    await new Promise(function (resolve, reject) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
